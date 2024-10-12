0b25309c69ec54d35cafcfa3ac8b1d235fbbe84b
psmela <pamelasalazar1091@gmail.com>
2024-10-12 12:46:33 -0300

    Agregar atributos de categoría, stock y descripción a los productos

--- models/producto.js
+++ models/producto.js
@@ -1,10 +1,27 @@
 const mongoose = require('mongoose');
 
 const productoSchema = new mongoose.Schema({
-    nombre: { type: String, required: true },
-    precio: { type: Number, required: true },
-    // Agrega otros campos que necesites
-});
+    nombre: { 
+        type: String, 
+        required: true 
+    },
+    precio: { 
+        type: Number, 
+        required: true 
+    },
+    categoria: { 
+        type: String, 
+        required: true 
+    },
+    stock: { 
+        type: Number, 
+        required: true 
+    },
+    descripcion: { 
+        type: String, 
+        required: true 
+    }
+}, { timestamps: true }); // Agrega timestamps para registrar automáticamente la creación y actualización
 
 const Producto = mongoose.model('Producto', productoSchema);
 
