4331caf7ad41875123626abe9041c981b9079ca9
benjikpop202 <benjaminsobarzo2018@gmail.com>
2025-01-10 13:06:04 -0300

    agregando cambios

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,7 +6,10 @@ const register = async (req, res)=>{
     const {userName, email, password} = req.body
     try {
        const UserGmail = await User.findOne({email})
-       if(UserGmail) res.status(400).json({message: 'el email ya esta en uso'})
+       if(UserGmail){
+        return res.status(400).json({message: 'el email ya esta en uso'})
+       }
+        
        const passwordHash = await bcrypt.hash(password, 10)
         const newUser = new User({
             userName,
