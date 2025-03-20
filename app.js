10a1647326af776e9da464b95590142ea4a6d33e
benjikpop202 <benjaminsobarzo2018@gmail.com>
2025-03-20 14:24:31 -0300

    arreglando cors x2

--- app.js
+++ app.js
@@ -12,19 +12,6 @@ app.use(cors({
   credentials: true, // Permite el envío de cookies y credenciales
 }));
 
-// Middleware para configurar manualmente los encabezados CORS (opcional si usas cors())
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(204); // Responde a las solicitudes preflight de CORS
-    }
-
-    next();
-});
 
 // Middlewares
 app.use(cookieParser());
